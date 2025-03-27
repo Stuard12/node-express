33661470321c5be89efcf5ca04351faed1e04889
Juan Pablo Hurtarte <48603168+Stuard12@users.noreply.github.com>
2025-03-26 20:17:15 -0600

    Update middleware.js

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,7 +1,7 @@
 import "./logger.js";
 
 export const unknownEndpoint = (req, res) => {
-	res.status(404).send({ error: `uknonwn endpoint` });
+	res.status(404).send({ error: `endpoint desconocido` });
 };
 
 export const errorHandler = (error, req, res, next) => {
