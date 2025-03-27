7aa13852559b90b5ca7c097965f5e10901bbaf5b
Juan Pablo Hurtarte <48603168+Stuard12@users.noreply.github.com>
2025-03-26 19:16:46 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,4 +14,4 @@ server.listen(PORT, () => {
 	logger.info(`Server listening at http://localhost:${PORT}`);
 	logger.info(`Access the root route at http://localhost:${PORT}/hello`);
 });
-console.log("🚀 Servidor reiniciado tercer intento");
+console.log("🚀 Servidor reiniciado cuarto intento  💥 Ahora sí deberías ver esto en los logs");
