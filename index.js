cabf375082178ad873438d19216225488dcdf6ba
Juan Pablo Hurtarte <48603168+Stuard12@users.noreply.github.com>
2025-03-29 22:11:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ import express from "express";
 
 const app = express();
 
-// Leer directamente la variable inyectada por Railway
-const mensaje = process.env.MI_VARIABLE || "No definida";
-
-console.log("🔔 Valor de MI_VARIABLE:", mensaje);
-
 app.get("/", (req, res) => {
-  res.send(`Valor de MI_VARIABLE: ${mensaje}`);
+  res.send(`Valor de MI_VARIABLE: ${process.env.MI_VARIABLE || "No definida"}`);
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log("Servidor escuchando en puerto",PORT;
 });
