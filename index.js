a4d5ba0215efd2aeac3ae3caff4c631c725f99cb
Juan Pablo Hurtarte <48603168+Stuard12@users.noreply.github.com>
2025-03-30 19:20:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 import app from './app.js';
-import express from "express";
-
-const app = express();
-
-const mensaje = process.env.MI_VARIABLE || "No definida";
-
-console.log("🔔 Valor de MI_VARIABLE:", mensaje);
-
-app.get("/", (req, res) => {
-  res.send(`Valor de MI_VARIABLE: ${mensaje}`);
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto`, PORT);
+    console.log(`✅ Servidor escuchando en puerto`, PORT);
 });
